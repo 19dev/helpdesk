f6ebb2aafbbeb552bf2f29ed699b5ad5fdd1a4cc
Faruk Celik <farukca@yahoo.com>
2014-07-07 00:16:10 +0300

    Search model name changes

--- app/models/helpdesk/ticket.rb
+++ app/models/helpdesk/ticket.rb
@@ -1,8 +1,8 @@
 module Helpdesk
   class Ticket < ActiveRecord::Base
 
-    include Tire::Model::Search
-    include Tire::Model::Callbacks
+    # include Tire::Model::Search
+    # include Tire::Model::Callbacks
 
     belongs_to :user, class_name: "Nimbos::User"
     belongs_to :assigned, class_name: "Nimbos::User"
@@ -19,42 +19,6 @@ module Helpdesk
     scope :latest, order("created_at desc")
 
     before_create :set_initials
-
-    after_touch() { tire.update_index }
-    index_name { "socialfreight-tickets" }#{Nimbos::Patron.current_id}
-    mapping do
-      indexes :id, type: :integer, index: :not_analyzed
-      indexes :patron_id, type: :integer, index: :not_analyzed
-      indexes :title, analyzer: 'snowball', boost: 100
-      indexes :desc, analyzer: 'snowball'
-      indexes :status, index: :not_analyzed
-      indexes :user do
-        indexes :name, analyzer: 'snowball'
-        indexes :avatar, index: :not_analyzed
-      end
-      indexes :assigned do
-        indexes :name, analyzer: 'snowball'
-      end
-      indexes :close_date, type: 'date', index: :not_analyzed
-      indexes :created_at, type: 'date', index: :not_analyzed
-    end
-
-    def to_indexed_json
-      to_json(include: { user: { only: [:name] }, assigned: { only: [:name] } })
-    end
-
-    # def self.search(query, page_no=1, per_page=10)
-    #   tire.search(load: false, page: page_no, per_page: per_page) do
-    #     query { string query } if query.present? #, default_operator: "AND"
-    #     filter :term, patron_id: Nimbos::Patron.current_id
-    #     sort  { by :created_at, "desc" } if query.blank?
-    #     #filter :range, published_at: {lte: Time.zone.now}
-    #   end
-    # end
-
-    # def self.paginate(options = {})
-    #   page(options[:page]).per(options[:per_page])
-    # end
 
     def self.search(search_id)
       search = Roster::Search.find(search_id)
@@ -86,3 +50,40 @@ private
     end
   end
 end
+
+
+    # after_touch() { tire.update_index }
+    # index_name { "socialfreight-tickets" }#{Nimbos::Patron.current_id}
+    # mapping do
+    #   indexes :id, type: :integer, index: :not_analyzed
+    #   indexes :patron_id, type: :integer, index: :not_analyzed
+    #   indexes :title, analyzer: 'snowball', boost: 100
+    #   indexes :desc, analyzer: 'snowball'
+    #   indexes :status, index: :not_analyzed
+    #   indexes :user do
+    #     indexes :name, analyzer: 'snowball'
+    #     indexes :avatar, index: :not_analyzed
+    #   end
+    #   indexes :assigned do
+    #     indexes :name, analyzer: 'snowball'
+    #   end
+    #   indexes :close_date, type: 'date', index: :not_analyzed
+    #   indexes :created_at, type: 'date', index: :not_analyzed
+    # end
+
+    # def to_indexed_json
+    #   to_json(include: { user: { only: [:name] }, assigned: { only: [:name] } })
+    # end
+
+    # def self.search(query, page_no=1, per_page=10)
+    #   tire.search(load: false, page: page_no, per_page: per_page) do
+    #     query { string query } if query.present? #, default_operator: "AND"
+    #     filter :term, patron_id: Nimbos::Patron.current_id
+    #     sort  { by :created_at, "desc" } if query.blank?
+    #     #filter :range, published_at: {lte: Time.zone.now}
+    #   end
+    # end
+
+    # def self.paginate(options = {})
+    #   page(options[:page]).per(options[:per_page])
+    # end
