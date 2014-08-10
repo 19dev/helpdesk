e8b80fd061a780c223c84f907eff16387f2d2a5d
Faruk Celik <farukca@yahoo.com>
2014-08-10 13:22:38 +0300

    total index changes

--- db/migrate/20140731153630_add_indexes_to_helpdesk_ticket_actions.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexesToHelpdeskTicketActions < ActiveRecord::Migration
-  def change
-  	add_index :helpdesk_ticket_actions, :ticket_id
-  end
-end
--- db/migrate/20140731153819_add_indexes_to_helpdesk_tickets.rb
+++ db/migrate/20140731153819_add_indexes_to_helpdesk_tickets.rb
@@ -1,6 +1,7 @@
 class AddIndexesToHelpdeskTickets < ActiveRecord::Migration
   def change
   	add_index :helpdesk_tickets, :patron_id
-  	add_index :helpdesk_tickets, [:title, :patron_id]
+  	add_index :helpdesk_tickets, :team_id
+  	add_index :helpdesk_tickets, :user_id
   end
 end
